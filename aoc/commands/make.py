b756aaa9107709d948499bd0d518a99acd7c552e
David <djphamill@gmail.com>
2024-11-28 19:40:27 +0000

    These are now being passed in as args.; no need for the palceholders anymore.

--- aoc/commands/make.py
+++ aoc/commands/make.py
@@ -4,9 +4,6 @@ from datetime import datetime
 this_year = datetime.now().strftime('%Y')
 
 def make(day, year=this_year): # TODO: do this better
-    day = '' # TODO
-    year = '' # TODO
-
     with open('/Users/david/adventofcode/aoc/commands/solution.py.mustache', 'r') as f:
         solution = chevron.render(f, {'mustache': 'World'})
 
