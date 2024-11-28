d8a3b8809aeee47921db3cf6996a1c172e7c49ab
David <djphamill@gmail.com>
2024-11-28 22:38:38 +0000

    Ensure days are two digits and years four.
    This will help with naming conventions.

--- aoc/run.py
+++ aoc/run.py
@@ -9,12 +9,24 @@ from commands.solve import solve
 
 this_year = datetime.now().strftime('%Y')
 
+class Year(int):
+
+    def __str__(self):
+        txt = super(int, self).__str__()
+        return txt.zfill(4)
+
+class Day(int):
+
+    def __str__(self):
+        txt = super(int, self).__str__()
+        return txt.zfill(2)
+
 def add_make_command(subparser: ArgumentParser) -> None:
     make_parser = subparser.add_parser("make", help="Make the files associated with a day's AOC")
 
-    make_parser.add_argument("day", type=int)
+    make_parser.add_argument("day", type=Day)
 
-    make_parser.add_argument("-y", "--year", type=int, default=this_year)
+    make_parser.add_argument("-y", "--year", type=Year, default=this_year)
     
     make_parser.set_defaults(func=make)
 
@@ -23,9 +35,9 @@ def add_solve_command(subparser: ArgumentParser) -> None:
     
     solve_parser.add_argument("input")
 
-    solve_parser.add_argument("day", type=int)
+    solve_parser.add_argument("day", type=Day)
     
-    solve_parser.add_argument("-y", "--year", type=int, default=this_year)
+    solve_parser.add_argument("-y", "--year", type=Year, default=this_year)
     solve_parser.set_defaults(func=solve)
 
 if __name__ == "__main__":
