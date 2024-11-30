537d35ffb60197acf29bc05eb350bedd03b2314b
David <djphamill@gmail.com>
2024-11-30 19:29:25 +0000

    Add example scenario.

--- _2020/day_01/tests/scenarios.py
+++ _2020/day_01/tests/scenarios.py
@@ -5,8 +5,13 @@ Scenarios for day {day}
 from common.scenario import Scenario
 
 part_01_scenarios = [
-    Scenario(input="",
-             output=""),
+    Scenario(input="""1721
+                    979
+                    366
+                    299
+                    675
+                    1456""",
+             output="514579"),
 ]
 
 part_02_scenarios = [
