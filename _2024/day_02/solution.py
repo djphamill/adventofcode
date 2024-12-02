bb3938c362296938769111ebc60b488587117b90
David <djphamill@gmail.com>
2024-12-02 08:36:28 +0000

    Starting point for day 2, 2024.

---
+++ _2024/day_02/solution.py
@@ -0,0 +1,35 @@
+"""
+Solution to day 02
+"""
+from typing import List
+
+def solution_part_01(input: str) -> str:
+    lines = [line.strip().split(" ") for line in input.split('\n') if line]O
+    
+    reports = []
+    for line in lines:
+        report = [int(level) for level in line] 
+        reports.append(report)
+
+    for report in reports:
+        pass
+
+def is_always_increasing(report: List[int]) -> bool:
+    return sorted(report) == report
+
+def is_always_decreasing(report: List[int]) -> bool:
+    return sorted(report, reverse=True) == report
+
+def is_difference_tolerable(report: List[int]) -> bool:
+    first_numbers = report[:-1]
+    second_numbers = report[1:]
+    for first_number, second_number in zip(first_numbers, second_numbers):
+        absolute_difference = abs(first_number - second_number)
+        if absolute_difference < 1:
+            return False
+        if absolute_difference > 3:
+            return False
+    return True
+
+def solution_part_02(input: str) -> str:
+    pass
---
+++ _2024/day_02/tests/scenarios.py
@@ -0,0 +1,20 @@
+"""
+Scenarios for day {day}
+"""
+
+from common.scenario import Scenario
+
+input="""7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9"""
+output="2"
+scenario = Scenario(input, output)
+part_01_scenarios = [scenario]
+
+input="""<paste here>"""
+output="<paste here>"
+scenario = Scenario(input, output)
+part_02_scenarios = [scenario]
